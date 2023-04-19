647db8c63145ed837c5526ef29dd1f663b3192ae
Panuwat010638 <122358819+Panuwat010638@users.noreply.github.com>
2023-04-19 19:14:24 +0700

    add icon

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ export default function Home() {
       <title>CLEO x StartClinic</title>
       <meta name="description" content="CLEO x StartClinic"/>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <link rel="icon" href="/favicon.ico" />
     </Head>
     <main >
       <div className='BG bg-white relative'>
